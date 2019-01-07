9007d6bc790338fe405603b050dd2fd83f5d5092
Marcuz <macoycorpuz@gmail.com>
2019-01-07 14:18:22 +0800

    Update: Bounding box and aligned face (livevid.py)

--- EmoPy/examples/livevid.py
+++ EmoPy/examples/livevid.py
@@ -10,6 +10,7 @@ from keras.layers import Dropout, Flatten, Dense
 
 from EmoPy.src.fermodel import FERModel
 from pkg_resources import resource_filename
+from EmoPy.library.align import AlignDlib
 
 def setROI(inputImg_size, roiwidth, roiheight):
 	w = inputImg_size[0]
@@ -27,95 +28,53 @@ def setROI(inputImg_size, roiwidth, roiheight):
 
 	return xo, xb, yo, yb
 
-def apply_fourier(img):
-	f = np.fft.fft2(img)
-	fshift = np.fft.fftshift(f)
-	return np.abs(fshift)
+def main():
 
-def hfd(magnitude, thresh_freq):
-	mag_size = np.int32(np.array(np.shape(magnitude)))
-	mag_h_size = np.int32(mag_size/2)
-	max_freq = np.sqrt(np.sum(np.square(mag_h_size)))
+	print("\n\n")
+	print("Press c to identify emotion or Esc to exit.")
+	print("The object should cover at least 80 percent of the captured image")
 
-	high_mag = 0
-	total_mag = 0
-	cutoff_freq = 2*max_freq/3
-	for i in range(0, mag_size[0]):
-		for j in range(0, mag_size[1]):
-			freq = np.sqrt(np.sum(np.square([i-mag_h_size[0], j-mag_h_size[1]])))
-			val = magnitude[i, j]
-			if freq >= cutoff_freq and val >= thresh_freq:
-				high_mag = high_mag + val
-			total_mag = total_mag + val
-
-	return 10*high_mag/(total_mag-np.max(np.max(magnitude[:]))), total_mag
-
-thresh_freq = 400
-thresh_hfd = 0.4
-thresh_fdd = 400
-
-target_emotions = ['calm', 'anger', 'happiness']
-model_emotions = FERModel(target_emotions, verbose=True)
-model = keras.models.load_model('weights-improvement-07-0.97.hdf5')
-print("\n\n\n")
-print("Press c to check whether the object is live or spoof or Esc to exit.")
-print("The object should cover at least 80 percent of the captured image")
-
-n = 3
-key = 0
-text = ""
-count = 0
-hfd_arr = [0]*n
-total_mag = [0]*n
-magnitude_arr = []
-
-video = cv2.VideoCapture(0)
-ret, inputImg = video.read()
-xo, xb, yo, yb = setROI(inputImg.shape, 300, 300)
-inputImg = inputImg[xo:xb, yo:yb]
-img = cv2.cvtColor(inputImg, cv2.COLOR_RGB2GRAY)
-
-while key!=27:
+	target_emotions = ['anger','happiness', 'calm']
+	model_emotions = FERModel(target_emotions, verbose=True)
+	model = keras.models.load_model('weights-improvement-07-0.97.hdf5')
+	
+	video = cv2.VideoCapture(0)
 	ret, inputImg = video.read()
+	xo, xb, yo, yb = setROI(inputImg.shape, 800, 800)
 	inputImg = inputImg[xo:xb, yo:yb]
+	alignment = AlignDlib('landmarks.dat')
+	boxColor = (255,0,0)
+	boxThickness = 3
+	text = ''
 
-	tensor_img = cv2.resize(inputImg, (256, 256))
-	tensor_img = tensor_img[:, :, 0]
-	text = model_emotions.predict_image_array(tensor_img)
-
-	if key==99:
-		tensor_img = cv2.resize(inputImg, (256, 256))
-		tensor_img_in = np.expand_dims(tensor_img, 0)
-		liveness_val = model.predict(tensor_img_in)
-
-		for index in range(n+2):
-			index = index%n
-			
-			ret, inputImg = video.read()
-
-			inputImg = inputImg[xo:xb, yo:yb]
-			img = cv2.cvtColor(inputImg, cv2.COLOR_RGB2GRAY)
-
-			magnitude = apply_fourier(img)
-			if count == 0:
-				magnitude_arr = [[[0]*np.size(img, 1)]*np.size(img, 0)]*n
-				magnitude_arr[index] = magnitude
-				count = 1
-			else:
-				magnitude_arr[index] = magnitude
-
-			hfd_arr[index], total_mag[index] = hfd(magnitude_arr[index], thresh_freq)
+	key = 0
+	while key!=27:
+		ret, inputImg = video.read()
+		inputImg = inputImg[xo:xb, yo:yb]
+		bb = alignment.getLargestFaceBoundingBox(inputImg)
 		
-		if np.median(hfd_arr)>=thresh_hfd and np.std(total_mag)>=thresh_fdd:
-			text = "LIVE"
-		elif liveness_val<0.5:
-			text = "LIVE"
-		else:
-			text = "SPOOF"
- 
-	inputImg = cv2.putText(inputImg, text, (50, 50), 0, 1.0, (0, 255, 0), 2) 
+		
+		if bb is not None:
+			pt1 = (bb.left(), bb.top())
+			pt2 = (bb.left()+bb.width(), bb.height()+bb.top())
+			cv2.rectangle(inputImg, pt1, pt2, boxColor, boxThickness)
+		
+		if key == 99:
+			if bb is not None:
+				imgAligned = alignment.align(96, inputImg, bb, landmarkIndices=AlignDlib.OUTER_EYES_AND_NOSE)
+				# imgAligned = tensor_img[:, :, 0]
+				text = model_emotions.predict_image_array(imgAligned)
 
-	cv2.imshow("image", inputImg)
-	key = cv2.waitKey(1)
+		
+		###########################################################################
+		#Insert Text
+		inputImg = cv2.putText(inputImg, text, (50, 50), 0, 1.0, (0, 255, 0), 2) 
+		#Title
+		cv2.imshow("Face Analysis", inputImg)
+		#Key check
+		key = cv2.waitKey(1)
 
-video.release()
+	video.release()
+
+if __name__ == '__main__':
+	main()
